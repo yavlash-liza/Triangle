0ed892ffe6298aea26851b39b7bbf9c6c11810ee
yavlash-liza <yavlashliza@gmail.com>
2021-11-29 00:40:04 +0300

    refactoring triangle factory

--- src/main/java/com/company/yavlash/factory/TriangleFactory.java
+++ src/main/java/com/company/yavlash/factory/TriangleFactory.java
@@ -14,42 +14,42 @@ public class TriangleFactory {
     private static final Logger logger = LogManager.getLogger();
     private static final int VALID_AMOUNT_OF_POINTS = 6;
 
-    public List<Triangle> getTriangle(List<double[]> parameters) throws TriangleException{
-        if(parameters == null || parameters.isEmpty()) {
-            logger.log(Level.WARN,"Invalid parameters {} in factory", parameters);
-            throw new TriangleException(String.format("Invalid triangle parameters! %s", parameters));
+    public List<Triangle> getTriangle(List<double[]> parameters) throws TriangleException {
+        if (parameters == null) {
+            logger.log(Level.ERROR, "Parameters are null in factory");
+            throw new TriangleException("Invalid triangle parameters!");
         }
         List<Triangle> triangles = new ArrayList<>();
-        for(double[] array : parameters) {
+        for (double[] array : parameters) {
             triangles.add(getTriangle(array));
         }
         return triangles;
     }
 
-    public Triangle getTriangle(double[] array) throws TriangleException {
-        if( array.length != VALID_AMOUNT_OF_POINTS) {
-            logger.log(Level.WARN,"Invalid count of points {} in factory", array.length);
+    private Triangle getTriangle(double[] array) throws TriangleException {
+        if (array.length != VALID_AMOUNT_OF_POINTS) {
+            logger.log(Level.ERROR, "Invalid count of points {} in factory", array.length);
             throw new TriangleException(String.format("Invalid count of points! %s", array.length));
         }
-        return  getTriangle(array[0], array[1], array[2], array[3], array[4], array[5]);
+        return getTriangle(array[0], array[1], array[2], array[3], array[4], array[5]);
     }
 
-    public Triangle getTriangle(double x1, double y1, double x2, double y2, double x3, double y3) throws TriangleException {
+    private Triangle getTriangle(double x1, double y1, double x2, double y2, double x3, double y3) throws TriangleException {
         Point pointA = new Point(x1, y1);
         Point pointB = new Point(x2, y2);
         Point pointC = new Point(x3, y3);
         Triangle triangle = getTriangle(pointA, pointB, pointC);
-        logger.log(Level.INFO,"New triangle {} was created", triangle);
+        logger.log(Level.INFO, "New triangle {} was created", triangle);
         return triangle;
     }
 
     public Triangle getTriangle(Point pointA, Point pointB, Point pointC) throws TriangleException {
-        if (pointA == null || pointB == null || pointC == null){
-            logger.log(Level.WARN,"Invalid parameters {}, {}, {} in factory", pointA, pointB, pointC);
+        if (pointA == null || pointB == null || pointC == null) {
+            logger.log(Level.ERROR, "Invalid parameters {}, {}, {} in factory", pointA, pointB, pointC);
             throw new TriangleException(String.format("Invalid triangle parameters! %s, %s, %s", pointA, pointB, pointC));
         }
         Triangle triangle = new Triangle(pointA, pointB, pointC);
-        logger.log(Level.INFO,"New triangle {} was created", triangle);
+        logger.log(Level.INFO, "New triangle {} was created", triangle);
         return triangle;
     }
 }
